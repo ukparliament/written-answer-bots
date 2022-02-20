02415d766f0bb1aae2fa4845f0e6e3b9e0bbe32b
Michael Smethurst <michael@smethur.st>
2022-02-20 16:11:32 +0000

    added home and answering bpody routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
+  get '/' => 'home#index', :as => 'home'
+  
   get 'members' => 'member#index', :as => 'member_list'
   
   get 'questions' => 'question#index', :as => 'question_list'
+  
+  get 'answering-bodies' => 'answering_body#index', :as => 'answering_body_list'
+  get 'answering-bodies/:answering_body' => 'answering_body#show', :as => 'answering_body_show'
 end
