218a7fe57432c2abceb04caff08f2c9acebfb0d8
Michael Smethurst <michael@smethur.st>
2022-02-20 16:12:02 +0000

    added new method for safe twitter reference

--- app/models/member.rb
+++ app/models/member.rb
@@ -9,4 +9,14 @@ class Member < ApplicationRecord
     end
     twitter_reference
   end
+  
+  def safe_twitter_reference
+    twitter_reference = ''
+    if self.handle
+      twitter_reference = '^' + self.handle
+    else
+      twitter_reference = '#' + self.display_name.gsub( ' ', '' )
+    end
+    twitter_reference
+  end
 end
