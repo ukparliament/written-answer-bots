4f017a76c647b43979dca8f5acfa309846d18943
Michael Smethurst <michael@smethur.st>
2022-02-23 15:59:29 +0000

    made safe twitter reference the member display name

--- app/models/member.rb
+++ app/models/member.rb
@@ -11,12 +11,6 @@ class Member < ApplicationRecord
   end
   
   def safe_twitter_reference
-    twitter_reference = ''
-    if self.handle
-      twitter_reference = '^' + self.handle
-    else
-      twitter_reference = '#' + self.display_name.gsub( ' ', '' )
-    end
-    twitter_reference
+    self.display_name
   end
 end
