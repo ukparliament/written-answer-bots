21660a59a0f38f4b7b612c1d2aa2ce5f8a79a61a
Michael Smethurst <michael@smethur.st>
2022-03-03 11:31:04 +0000

    order answers by corrected date, then answer date

--- app/models/answering_body.rb
+++ app/models/answering_body.rb
@@ -1,4 +1,8 @@
 class AnsweringBody < ApplicationRecord
   
-  has_many :questions, -> { limit( 20 ) }
+  has_many :questions,
+    -> { order( 'questions.date_answer_corrected desc, questions.date_answered desc' ).limit( 20 ) }
 end
+
+
+
