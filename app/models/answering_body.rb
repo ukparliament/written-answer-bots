b37ba684ca49601cb816b39517b4d39267a167ea
Michael Smethurst <michael@smethur.st>
2022-04-07 13:43:03 +0100

    new method to return untweeted answers

--- app/models/answering_body.rb
+++ app/models/answering_body.rb
@@ -2,6 +2,10 @@ class AnsweringBody < ApplicationRecord
   
   has_many :questions,
     -> { order( 'questions.pertinent_date desc' ).limit( 20 ) }
+    
+  def untweeted_answers
+    Question.all.where( 'answering_body_id = ?', self.id ).where( 'tweeted is false' )
+  end
 end
 
 
