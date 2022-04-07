78228db53f4daddac8ec28be8992a05af1502a92
Michael Smethurst <michael@smethur.st>
2022-04-07 11:38:48 +0100

    updated to list answers by pertinent date being either the date of answer or date of correction

--- app/models/answering_body.rb
+++ app/models/answering_body.rb
@@ -1,7 +1,7 @@
 class AnsweringBody < ApplicationRecord
   
   has_many :questions,
-    -> { order( 'questions.date_answer_corrected desc, questions.date_answered desc' ).limit( 20 ) }
+    -> { order( 'questions.pertinent_date desc' ).limit( 20 ) }
 end
 
 
