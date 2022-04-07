0adaaa7b09fefabf2234dc6a3c9b3916fdfb5f09
Michael Smethurst <michael@smethur.st>
2022-04-07 11:37:16 +0100

    removed safe twitter reference method. Use display name, not hashtag for twitter reference if no handle present

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,15 +2,18 @@ class Member < ApplicationRecord
   
   def twitter_reference
     twitter_reference = ''
+    # If the Member has a Twitter handle ...
     if self.handle
+      
+      # ... set the Twitter reference to @ mention that handle.
       twitter_reference = '@' + self.handle
+      
+    # Otherwise, if the Member does not have a Twitter handle ...
     else
-      twitter_reference = '#' + self.display_name.gsub( ' ', '' )
+      
+      # ... set the Twitter reference to their display name.
+      twitter_reference = self.display_name
     end
     twitter_reference
-  end
-  
-  def safe_twitter_reference
-    self.display_name
   end
 end
