aa6f5d9bfd4c55dab6d327cd9faae69d8842dcf5
Michael Smethurst <michael@smethur.st>
2022-07-25 10:45:55 +0100

    removed limit of 20 latest

--- app/models/answering_body.rb
+++ app/models/answering_body.rb
@@ -1,7 +1,7 @@
 class AnsweringBody < ApplicationRecord
   
   has_many :questions,
-    -> { order( 'questions.pertinent_date desc' ).limit( 20 ) }
+    -> { order( 'questions.pertinent_date desc' ) }
     
   def untweeted_answers
     Question.all.where( 'answering_body_id = ?', self.id ).where( 'tweeted is false' )
