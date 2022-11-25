17d484936005a221fea786337861e70cc7f471b9
Michael Smethurst <michael@smethur.st>
2022-11-25 10:30:22 +0000

    new controller and basic views to run scripts as modules

---
+++ app/controllers/task_controller.rb
@@ -0,0 +1,31 @@
+# We require the code.
+require 'import/import_members'
+require 'import/import_questions'
+require 'import/cleanup'
+require 'tweet/tweet'
+
+# We include code from modules.
+include IMPORT_MEMBERS
+include IMPORT_QUESTIONS
+include CLEANUP
+include TWEET
+
+class TaskController < ApplicationController
+  
+  def task_import_members
+    import_members
+  end
+  
+  def task_import_questions
+    import_questions
+  end
+  
+  def task_cleanup
+    cleanup
+  end
+  
+  def task_tweet
+    answering_body_id = params[:answering_body]
+    tweet_set_up( answering_body_id )
+  end
+end
