de15dff113c61ba43b82c8de955a0cf2265cdb06
Michael Smethurst <michael@smethur.st>
2022-11-29 14:18:22 +0000

    made tweet urls full paths not query strings

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   get 'tasks/import-members' => 'task#task_import_members', :as => 'task_import_members'
   get 'tasks/import-questions' => 'task#task_import_questions', :as => 'task_import_questions'
   get 'tasks/cleanup' => 'task#task_cleanup', :as => 'task_cleanup'
-  get 'tasks/tweet' => 'task#task_tweet', :as => 'task_tweet'
+  get 'tasks/tweet/:answering_body' => 'task#task_tweet', :as => 'task_tweet'
 end
