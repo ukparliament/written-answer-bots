ee99db40c4cd4885e9698a27a306b29b091b4b56
fantasticlife <michael@smethur.st:wq>
2023-03-03 10:32:42 +0000

    should now work on mastodon with no twitter account set up

--- lib/tweet/tweet.rb
+++ lib/tweet/tweet.rb
@@ -47,134 +47,144 @@ module TWEET
       access_secret = ENV['OPM_ACCESS_SECRET']
       bearer_token = ENV['OPM_BEARER']
     when 88
-      consumer_key = ENV['AG_CONSUMER_KEY']
-      consumer_secret = ENV['AG_CONSUMER_SECRET']
-      access_token = ENV['AG_ACCESS_TOKEN']
-      access_secret = ENV['AG_ACCESS_SECRET']
+      #consumer_key = ENV['AG_CONSUMER_KEY']
+      #consumer_secret = ENV['AG_CONSUMER_SECRET']
+      #access_token = ENV['AG_ACCESS_TOKEN']
+      #access_secret = ENV['AG_ACCESS_SECRET']
       bearer_token = ENV['AG_BEARER']
     when 53
-      consumer_key = ENV['CO_CONSUMER_KEY']
-      consumer_secret = ENV['CO_CONSUMER_SECRET']
-      access_token = ENV['CO_ACCESS_TOKEN']
-      access_secret = ENV['CO_ACCESS_SECRET']
+      #consumer_key = ENV['CO_CONSUMER_KEY']
+      #consumer_secret = ENV['CO_CONSUMER_SECRET']
+      #access_token = ENV['CO_ACCESS_TOKEN']
+      #access_secret = ENV['CO_ACCESS_SECRET']
       bearer_token = ENV['CO_BEARER']
     when 9
-      consumer_key = ENV['CC_CONSUMER_KEY']
-      consumer_secret = ENV['CC_CONSUMER_SECRET']
-      access_token = ENV['CC_ACCESS_TOKEN']
-      access_secret = ENV['CC_ACCESS_SECRET']
+      #consumer_key = ENV['CC_CONSUMER_KEY']
+      #consumer_secret = ENV['CC_CONSUMER_SECRET']
+      #access_token = ENV['CC_ACCESS_TOKEN']
+      #access_secret = ENV['CC_ACCESS_SECRET']
       bearer_token = ENV['CC_BEARER']
     when 13
-      consumer_key = ENV['DEFRA_CONSUMER_KEY']
-      consumer_secret = ENV['DEFRA_CONSUMER_SECRET']
-      access_token = ENV['DEFRA_ACCESS_TOKEN']
-      access_secret = ENV['DEFRA_ACCESS_SECRET']
+      #consumer_key = ENV['DEFRA_CONSUMER_KEY']
+      #consumer_secret = ENV['DEFRA_CONSUMER_SECRET']
+      #access_token = ENV['DEFRA_ACCESS_TOKEN']
+      #access_secret = ENV['DEFRA_ACCESS_SECRET']
       bearer_token = ENV['DEFRA_BEARER']
     when 27
-      consumer_key = ENV['DFT_CONSUMER_KEY']
-      consumer_secret = ENV['DFT_CONSUMER_SECRET']
-      access_token = ENV['DFT_ACCESS_TOKEN']
-      access_secret = ENV['DFT_ACCESS_SECRET']
+      #consumer_key = ENV['DFT_CONSUMER_KEY']
+      #consumer_secret = ENV['DFT_CONSUMER_SECRET']
+      #access_token = ENV['DFT_ACCESS_TOKEN']
+      #access_secret = ENV['DFT_ACCESS_SECRET']
       bearer_token = ENV['DFT_BEARER']
     when 29
-      consumer_key = ENV['DWP_CONSUMER_KEY']
-      consumer_secret = ENV['DWP_CONSUMER_SECRET']
-      access_token = ENV['DWP_ACCESS_TOKEN']
-      access_secret = ENV['DWP_ACCESS_SECRET']
+      #consumer_key = ENV['DWP_CONSUMER_KEY']
+      #consumer_secret = ENV['DWP_CONSUMER_SECRET']
+      #access_token = ENV['DWP_ACCESS_TOKEN']
+      #access_secret = ENV['DWP_ACCESS_SECRET']
       bearer_token = ENV['DWP_BEARER']
     when 29
-      consumer_key = ENV['DHSC_CONSUMER_KEY']
-      consumer_secret = ENV['DHSC_CONSUMER_SECRET']
-      access_token = ENV['DHSC_ACCESS_TOKEN']
-      access_secret = ENV['DHSC_ACCESS_SECRET']
+      #consumer_key = ENV['DHSC_CONSUMER_KEY']
+      #consumer_secret = ENV['DHSC_CONSUMER_SECRET']
+      #access_token = ENV['DHSC_ACCESS_TOKEN']
+      #access_secret = ENV['DHSC_ACCESS_SECRET']
       bearer_token = ENV['DHSC_BEARER']
     when 1
-      consumer_key = ENV['HO_CONSUMER_KEY']
-      consumer_secret = ENV['HO_CONSUMER_SECRET']
-      access_token = ENV['HO_ACCESS_TOKEN']
-      access_secret = ENV['HO_ACCESS_SECRET']
+      #consumer_key = ENV['HO_CONSUMER_KEY']
+      #consumer_secret = ENV['HO_CONSUMER_SECRET']
+      #access_token = ENV['HO_ACCESS_TOKEN']
+      #access_secret = ENV['HO_ACCESS_SECRET']
       bearer_token = ENV['HO_BEARER']
     when 18
-      consumer_key = ENV['HCC_CONSUMER_KEY']
-      consumer_secret = ENV['HCC_CONSUMER_SECRET']
-      access_token = ENV['HCC_ACCESS_TOKEN']
-      access_secret = ENV['HCC_ACCESS_SECRET']
+      #consumer_key = ENV['HCC_CONSUMER_KEY']
+      #consumer_secret = ENV['HCC_CONSUMER_SECRET']
+      #access_token = ENV['HCC_ACCESS_TOKEN']
+      #access_secret = ENV['HCC_ACCESS_SECRET']
       bearer_token = ENV['HCC_BEARER']
     when 34
-      consumer_key = ENV['LH_CONSUMER_KEY']
-      consumer_secret = ENV['LH_CONSUMER_SECRET']
-      access_token = ENV['LH_ACCESS_TOKEN']
-      access_secret = ENV['LH_ACCESS_SECRET']
+      #consumer_key = ENV['LH_CONSUMER_KEY']
+      #consumer_secret = ENV['LH_CONSUMER_SECRET']
+      #access_token = ENV['LH_ACCESS_TOKEN']
+      #access_secret = ENV['LH_ACCESS_SECRET']
       bearer_token = ENV['LH_BEARER']
     when 92
-      consumer_key = ENV['HHL_CONSUMER_KEY']
-      consumer_secret = ENV['HHL_CONSUMER_SECRET']
-      access_token = ENV['HHL_ACCESS_TOKEN']
-      access_secret = ENV['HHL_ACCESS_SECRET']
+      #consumer_key = ENV['HHL_CONSUMER_KEY']
+      #consumer_secret = ENV['HHL_CONSUMER_SECRET']
+      #access_token = ENV['HHL_ACCESS_TOKEN']
+      #access_secret = ENV['HHL_ACCESS_SECRET']
       bearer_token = ENV['HHL_BEARER']
     when 91
-      consumer_key = ENV['MEC_CONSUMER_KEY']
-      consumer_secret = ENV['MEC_CONSUMER_SECRET']
-      access_token = ENV['MEC_ACCESS_TOKEN']
-      access_secret = ENV['MEC_ACCESS_SECRET']
+      #consumer_key = ENV['MEC_CONSUMER_KEY']
+      #consumer_secret = ENV['MEC_CONSUMER_SECRET']
+      #access_token = ENV['MEC_ACCESS_TOKEN']
+      #access_secret = ENV['MEC_ACCESS_SECRET']
       bearer_token = ENV['MEC_BEARER']
     when 11
-      consumer_key = ENV['MOD_CONSUMER_KEY']
-      consumer_secret = ENV['MOD_CONSUMER_SECRET']
-      access_token = ENV['MOD_ACCESS_TOKEN']
-      access_secret = ENV['MOD_ACCESS_SECRET']
+      #consumer_key = ENV['MOD_CONSUMER_KEY']
+      #consumer_secret = ENV['MOD_CONSUMER_SECRET']
+      #access_token = ENV['MOD_ACCESS_TOKEN']
+      #access_secret = ENV['MOD_ACCESS_SECRET']
       bearer_token = ENV['MOD_BEARER']
     when 54
-      consumer_key = ENV['MOJ_CONSUMER_KEY']
-      consumer_secret = ENV['MOJ_CONSUMER_SECRET']
-      access_token = ENV['MOJ_ACCESS_TOKEN']
-      access_secret = ENV['MOJ_ACCESS_SECRET']
+      #consumer_key = ENV['MOJ_CONSUMER_KEY']
+      #consumer_secret = ENV['MOJ_CONSUMER_SECRET']
+      #access_token = ENV['MOJ_ACCESS_TOKEN']
+      #access_secret = ENV['MOJ_ACCESS_SECRET']
       bearer_token = ENV['MOJ_BEARER']
     when 21
-      consumer_key = ENV['NIO_CONSUMER_KEY']
-      consumer_secret = ENV['NIO_CONSUMER_SECRET']
-      access_token = ENV['NIO_ACCESS_TOKEN']
-      access_secret = ENV['NIO_ACCESS_SECRET']
+      #consumer_key = ENV['NIO_CONSUMER_KEY']
+      #consumer_secret = ENV['NIO_CONSUMER_SECRET']
+      #access_token = ENV['NIO_ACCESS_TOKEN']
+      #access_secret = ENV['NIO_ACCESS_SECRET']
       bearer_token = ENV['NIO_BEARER']
     when 2
-      consumer_key = ENV['SO_CONSUMER_KEY']
-      consumer_secret = ENV['SO_CONSUMER_SECRET']
-      access_token = ENV['SO_ACCESS_TOKEN']
-      access_secret = ENV['SO_ACCESS_SECRET']
+      #consumer_key = ENV['SO_CONSUMER_KEY']
+      #consumer_secret = ENV['SO_CONSUMER_SECRET']
+      #access_token = ENV['SO_ACCESS_TOKEN']
+      #access_secret = ENV['SO_ACCESS_SECRET']
       bearer_token = ENV['SO_BEARER']
     when 90
-      consumer_key = ENV['SCIPSA_CONSUMER_KEY']
-      consumer_secret = ENV['SCIPSA_CONSUMER_SECRET']
-      access_token = ENV['SCIPSA_ACCESS_TOKEN']
-      access_secret = ENV['SCIPSA_ACCESS_SECRET']
+      #consumer_key = ENV['SCIPSA_CONSUMER_KEY']
+      #consumer_secret = ENV['SCIPSA_CONSUMER_SECRET']
+      #access_token = ENV['SCIPSA_ACCESS_TOKEN']
+      #access_secret = ENV['SCIPSA_ACCESS_SECRET']
       bearer_token = ENV['SCIPSA_BEARER']
     when 204
-      consumer_key = ENV['SDS_CONSUMER_KEY']
-      consumer_secret = ENV['SDS_CONSUMER_SECRET']
-      access_token = ENV['SDS_ACCESS_TOKEN']
-      access_secret = ENV['SDS_ACCESS_SECRET']
+      #consumer_key = ENV['SDS_CONSUMER_KEY']
+      #consumer_secret = ENV['SDS_CONSUMER_SECRET']
+      #access_token = ENV['SDS_ACCESS_TOKEN']
+      #access_secret = ENV['SDS_ACCESS_SECRET']
       bearer_token = ENV['SDS_BEARER']
     when 14
-      consumer_key = ENV['HMT_CONSUMER_KEY']
-      consumer_secret = ENV['HMT_CONSUMER_SECRET']
-      access_token = ENV['HMT_ACCESS_TOKEN']
-      access_secret = ENV['HMT_ACCESS_SECRET']
+      #consumer_key = ENV['HMT_CONSUMER_KEY']
+      #consumer_secret = ENV['HMT_CONSUMER_SECRET']
+      #access_token = ENV['HMT_ACCESS_TOKEN']
+      #access_secret = ENV['HMT_ACCESS_SECRET']
       bearer_token = ENV['HMT_BEARER']
     when 28
-      consumer_key = ENV['WO_CONSUMER_KEY']
-      consumer_secret = ENV['WO_CONSUMER_SECRET']
-      access_token = ENV['WO_ACCESS_TOKEN']
-      access_secret = ENV['WO_ACCESS_SECRET']
+      #consumer_key = ENV['WO_CONSUMER_KEY']
+      #consumer_secret = ENV['WO_CONSUMER_SECRET']
+      #access_token = ENV['WO_ACCESS_TOKEN']
+      #access_secret = ENV['WO_ACCESS_SECRET']
       bearer_token = ENV['WO_BEARER']
     end
     
-    # We authenticate to Twitter.
-    client = Tweetkit::Client.new(
-      consumer_key: consumer_key,
-      consumer_secret: consumer_secret,
-      access_token: access_token,
-      access_token_secret: access_secret
-    )
+    # If we've set the Twitter credentials ...
+    if consumer_key ...
+    
+      # ... we authenticate to Twitter.
+      client = Tweetkit::Client.new(
+        consumer_key: consumer_key,
+        consumer_secret: consumer_secret,
+        access_token: access_token,
+        access_token_secret: access_secret
+      )
+      
+    # Otherwise ...
+    else
+      
+      # ... we set the client to nil.
+      client = nil
+    end
     
     # We find the answering body by its MNIS ID.
     answering_body = AnsweringBody.find_by_mnis_id( answering_body_id )
@@ -195,8 +205,12 @@ module TWEET
     # For each answer ...
     answers.each do |answer|
       
-      # ... we post the tweet.
-      client.post_tweet( text: answer.safe_tweet_text )
+      # ... if we've authenticated to Twitter ...
+      if client
+        
+        # ... we post the tweet.
+        client.post_tweet( text: answer.safe_tweet_text )
+      end
       
       # If a bearer token has been passed ...
       if bearer_token
