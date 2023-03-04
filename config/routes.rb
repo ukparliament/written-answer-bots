edc36f7e169e06056f67fae0334ab65b935980c0
fantasticlife <michael@smethur.st:wq>
2023-03-04 14:40:57 +0000

    added a duplicate url for /answering-bodies/:answering_body.rss because logs show someone is subscribing and we don't want to break that

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,12 @@ Rails.application.routes.draw do
   get 'written-answers/answering-bodies' => 'answering_body#index', :as => 'answering_body_list'
   get 'written-answers/answering-bodies/:answering_body' => 'answering_body#show', :as => 'answering_body_show'
   
+  # An unfortuante duplication of the above.
+  # Logs show someone is subscribing to the RSS at:
+  # https://written-questions.herokuapp.com/answering-bodies/{answering_body.id}.rss
+  # Which we don't want to break.
+  get 'answering-bodies/:answering_body' => 'answering_body#show', :as => 'answering_body_show_old_url'
+  
   get 'written-answers/tasks' => 'task#index', :as => 'task_index'
   get 'written-answers/tasks/import-members' => 'task#task_import_members', :as => 'task_import_members'
   get 'written-answers/tasks/import-questions' => 'task#task_import_questions', :as => 'task_import_questions'
