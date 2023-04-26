1d808c3d33c77c36f4057aa19c93579f50b2bb7c
fantasticlife <michael@smethur.st:wq>
2023-04-26 12:05:39 +0100

    hashtagified IDMS headings

--- app/models/question.rb
+++ app/models/question.rb
@@ -14,6 +14,58 @@ class Question < ApplicationRecord
     Member.all.where( "mnis_id = #{self.correcting_member_id}" ).first
   end
   
+  # A method to generate hashtags for a question.
+  def hashtags
+    
+    # We create a string to hold the hashtags
+    hashtags = ''
+  
+    # We split the question heading on any colon to get the subjects.
+    self.heading.split( ':' ).each do |subject|
+      
+      # We create a string to hold the subject hastag.
+      subject_hashtag = ''
+      
+      # We split the subject on spaces to tokenise into words.
+      subject.split( ' ' ).each do |word|
+        
+        # If the word is longer than one character ...
+        if word.length > 1
+          
+          # Wishing to avoid turning BBC into Bbc.
+          # ... unless the second letter of the word is uppercase ...
+          unless is_upper?( word[1] )
+            
+            # ... we capatlise the first letter of the word.
+            word.capitalize!
+          end
+        end
+        
+        # We add the word to the subject hashtag.
+        subject_hashtag += word
+      end
+      
+      # We hastag-ify the subject hashtag.
+      subject_hashtag = '#' + subject_hashtag
+      
+      # We add the subject hashtag to the hashtags with a space.
+      hashtags += subject_hashtag + ' '
+    end
+    
+    # We return the hashtags.
+    hashtags
+  end
+  
+  # A method to check if a character is lowercase.
+  def is_lower?( character )
+    character >= 'a' && character <= 'z'
+  end
+
+  # A method to check if a character is uppercase.
+  def is_upper?( character )
+    ! is_lower?( character )
+  end
+  
   def title
     title = ''
     if self.is_correcting_answer
@@ -22,7 +74,7 @@ class Question < ApplicationRecord
       title +=  "A "
     end
     title += "question "
-    title += "on '#{self.heading}' " if self.heading
+    title += "on #{self.hashtags}" if self.heading
     title += "tabled "
     title += "by #{self.asking_member.display_name} " if self.asking_member
     title += "on #{self.date_tabled.strftime( '%d-%m-%Y' )} has been "
