ca0a54214a185dba6ccddec467bd8feb30ded474
fantasticlife <michael@smethur.st:wq>
2023-12-12 11:36:16 +0000

    new methods for mastodon post text, bluesky post text and url

--- app/models/written_statement.rb
+++ app/models/written_statement.rb
@@ -2,4 +2,27 @@ class WrittenStatement < ApplicationRecord
   
   belongs_to :member
   belongs_to :answering_body
+  
+  def mastondon_post_text
+    post_text = ''
+    post_text += self.title + '. ' if self.title
+    post_text += 'Statement made by ' + self.member_name + ' ' if self.member_name
+    post_text += '(' + self.member_role + ') ' if self.member_role
+    post_text += 'on behalf of the ' + self.answering_body_name + ' ' if self.answering_body_name
+    post_text += self.url
+    post_text
+  end
+  
+  def bluesky_post_text
+    post_text = ''
+    post_text += self.title + '. ' if self.title
+    post_text += 'Statement made by ' + self.member_name + ' ' if self.member_name
+    post_text += 'on behalf of the ' + self.answering_body_name + ' ' if self.answering_body_name
+    post_text += self.url
+    post_text
+  end
+  
+  def url
+    "https://questions-statements.parliament.uk/written-statements/detail/#{self.made_on}/#{self.uin}"
+  end
 end
