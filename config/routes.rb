916552d9a9dd9d42e9991d4ee567cefbe978c69a
fantasticlife <michael@smethur.st>
2025-09-01 16:04:55 +0100

    added separate routes for answers and statements for an answering body

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,37 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
+  mount LibraryDesign::Engine => "/library_design"
+  
   get '/' => 'home#index', :as => 'root'
   get 'written-answers' => 'home#index', :as => 'home'
   
-  get 'written-answers/members' => 'member#index', :as => 'member_list'
-  
   get 'written-answers/answers' => 'answer#index', :as => 'answer_list'
-  get 'written-answers/answers/:answer' => 'answer#show', :as => 'answer_show'
   
   get 'written-answers/written-statements' => 'written_statement#index', :as => 'written_statement_list'
   
   get 'written-answers/answering-bodies' => 'answering_body#index', :as => 'answering_body_list'
   get 'written-answers/answering-bodies/:answering_body' => 'answering_body#show', :as => 'answering_body_show'
   
+  get 'written-answers/answering-bodies/:answering_body/answers' => 'answering_body_answer#index', :as => 'answering_body_answer_list'
+  
+  get 'written-answers/answering-bodies/:answering_body/written-statements' => 'answering_body_written_statement#index', :as => 'answering_body_written_statement_list'
+  
   get 'written-answers/meta' => 'meta#index', :as => 'meta_list'
   get 'written-answers/meta/cookies' => 'meta#cookies', :as => 'meta_cookies'
+  
+  
+  
+  
+  
+  
+  # No longer linked to.
+  get 'written-answers/answers/:answer' => 'answer#show', :as => 'answer_show'
+  get 'written-answers/members' => 'member#index', :as => 'member_list'
   
   # An unfortuante duplication of the above.
   # Logs show someone is subscribing to the RSS at:
   # https://written-questions.herokuapp.com/answering-bodies/{answering_body.id}.rss
   # Which we don't want to break.
   get 'answering-bodies/:answering_body' => 'answering_body#show', :as => 'answering_body_show_old_url'
-  
-  get 'written-answers/tasks' => 'task#index', :as => 'task_index'
-  get 'written-answers/tasks/import-members' => 'task#task_import_members', :as => 'task_import_members'
-  get 'written-answers/tasks/import-questions' => 'task#task_import_questions', :as => 'task_import_questions'
-  get 'written-answers/tasks/cleanup' => 'task#task_cleanup', :as => 'task_cleanup'
-  get 'written-answers/tasks/tweet/:answering_body' => 'task#task_tweet', :as => 'task_tweet'
 end
